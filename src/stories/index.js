4c30881986ed3d7b1cb7f0de8d4f23b374ba3659
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2018-11-30 16:08:26 +0000

    Add Storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import Title from '../components/Title';
+import Header from '../components/Header';
+import SearchForm from '../components/SearchForm';
 
-import { Button, Welcome } from '@storybook/react/demo';
+storiesOf('App', module)
+.add('with header',() => (<Header />));
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+storiesOf('Header', module)
+.add('with title', () => (
+  <Title />
+))
+.add('with SearchForm', () => (
+  <SearchForm />
+));
